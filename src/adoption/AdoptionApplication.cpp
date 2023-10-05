69cff2428ff2ec34d8dfe0dd0c391c80f2d1c8f0
ryantoxx <114425150+ryantoxx@users.noreply.github.com>
2023-10-05 10:06:37 +0300

    Delete src directory

--- src/adoption/AdoptionApplication.cpp
+++
@@ -1,8 +0,0 @@
-#include "AdoptionApplication.h"
-
-AdoptionApplication::AdoptionApplication(Adopter* adopter, const std::string& animalName)
-    : adopter(adopter), animalName(animalName) {}
-
-void AdoptionApplication::display() const {
-   std::cout << "Adopter: " << adopter->getName() << ", Animal: " << animalName << std::endl;
-}
--- src/animals/Cat.cpp
+++
@@ -1,8 +0,0 @@
-#include "Cat.h"
-#include <iostream>
-
-Cat::Cat(const std::string& name, int age) : Animal(name, age) {}
-
-void Cat::makeSound() const {
-    std::cout << "Meow! Meow!" << std::endl;
-}
