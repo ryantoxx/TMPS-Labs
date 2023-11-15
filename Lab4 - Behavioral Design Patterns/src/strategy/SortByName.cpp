813cc30cc69c87478804af5344b2f2ebf931327c
ryantoxx <114425150+ryantoxx@users.noreply.github.com>
2023-11-15 17:00:23 +0200

    add: lab5

--- Lab4 - Behavioral Design Patterns/src/strategy/SortByName.cpp
+++ Lab4 - Behavioral Design Patterns/src/strategy/SortByName.cpp
@@ -4,5 +4,6 @@
 void SortByName::sort(std::vector<IAnimal*>& animals) const {
     std::sort(animals.begin(), animals.end(), [](const IAnimal* a, const IAnimal* b) {
         return a->getName() < b->getName();
+    
     });
 }
